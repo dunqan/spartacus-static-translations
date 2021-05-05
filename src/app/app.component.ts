1851812309d09951b4bbb3eb8d38fdad90fcbe42
dunqan <dunqan@w9.pl>
2021-05-05 09:12:58 +0200

    debug logs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, Inject, Optional} from '@angular/core';
+import {SERVER_REQUEST_ORIGIN} from '@spartacus/core';
+import {REQUEST} from '@nguniversal/express-engine/tokens';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'confo-test3-app';
+  debugData = {};
+
+  constructor(@Optional() @Inject(SERVER_REQUEST_ORIGIN) requestOrigin,
+              @Optional() @Inject(REQUEST) request) {
+    this.debugData = { requestOrigin,
+      hostName: request?.hostname,
+      originalUrl: request?.originalUrl,
+      forwardedHost: request?.get('X-Forwarded-Host'),
+      host: request?.get('host')
+    };
+  }
 }
